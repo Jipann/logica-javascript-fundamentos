32b9a1a20c5b701c16ce31cbae3181ca115d1ea3
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-08-21 15:48:53 -0300

    feat: adiciona modulo 01 e exercicio 01 de soma

---
+++ Estruturas-Sequenciais/01-soma-dois-numeros.js
@@ -0,0 +1,9 @@
+// Exercício 01: Soma de dois números
+// Objetivo: Declarar duas variáveis, somá-las e exibir o resultado no console.
+
+const numeroA = 15
+const numeroB = 25
+
+const soma = numeroA + numeroB
+
+console.log(`A soma entre ${numeroA} e ${numeroB} é: ${soma}`)
