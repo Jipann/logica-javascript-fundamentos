6324a4785f75a8bd67b55d9683c41a7a907ac139
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-08-24 19:18:17 -0300

    feat: adiciona exercicio conversor de distancia

---
+++ Estruturas-Sequenciais/12-calcular-idade.js
@@ -0,0 +1,15 @@
+/**
+ * receba um ano de nascimento e informe a idade 
+ */
+
+
+let anoNascimento = 1989
+let anoAtual = 2026
+
+console.log(`sua idade é = ${anoAtual - anoNascimento } anos`)
+
+
+let hora = 3
+hora = hora * 60
+
+console.log('em minutos:', hora)
---
+++ Estruturas-Sequenciais/13-conversor-distancia.js
@@ -0,0 +1,15 @@
+/**
+ * Conversão de distância
+
+Quilômetros → metros.
+Metros → centímetros.
+ */
+
+let quilometos = 500
+
+const metros = quilometos * 1000
+
+const centimetros = metros * 100
+
+console.log(metros)
+console.log(centimetros)
