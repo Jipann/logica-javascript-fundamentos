b237dced9f15dba69c33554b391d4490f2e59d68
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-08-24 15:45:16 -0300

    feat: adiciona exercicio calculadora simples

---
+++ Estruturas-Sequenciais/11-calculadoraSimples.js
@@ -0,0 +1,19 @@
+/**
+ * faça uma calculadora simples com as principais operações
+ * soma, subtração, multiplicação e divisão, receba dois numeros
+ */
+
+
+let numero1 = 10
+let numero2 = 5
+
+
+let soma = numero1 + numero2
+let sub = numero1 - numero2
+let mult = numero1 * numero2
+let div = numero1 / numero2
+
+console.log(`o valor da soma ${numero1} + ${numero2} = ${soma}`)
+console.log(`o valor da subtração ${numero1} - ${numero2} = ${sub}`)
+console.log(`o valor da multiplicação ${numero1} * ${numero2} = ${mult}`)
+console.log(`o valor da divisão ${numero1} / ${numero2} = ${div}`)
