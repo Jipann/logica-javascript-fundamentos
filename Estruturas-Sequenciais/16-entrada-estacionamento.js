93974720c2ff06df4cde07b2baabd561085bb46b
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-08-25 14:58:14 -0300

    feat: adiciona exercicio entrada estacionamento

--- Estruturas-Sequenciais/16-entrada-estacionamento.js
+++ Estruturas-Sequenciais/16-entrada-estacionamento.js
@@ -35,7 +35,7 @@ E saiu:
 17:15
  */
 
-const horaEntrada = 16
+/*const horaEntrada = 16
 const minutosEntrada = 15
 const horasaida = 17
 const minutosSaida = 15
@@ -49,4 +49,76 @@ const tempoNoEstacionamento = horarioSaida - horarioEntrada
 
 const tempoEmHoras = tempoNoEstacionamento / 60
 
-console.log(`Entrei às ${horaEntrada}:${minutosEntrada} e saí às ${horasaida}:${minutosSaida}.Fiquei ${tempoEmHoras} hora.`)
+console.log(`Entrei às ${horaEntrada}:${minutosEntrada} e saí às ${horasaida}:${minutosSaida}.Fiquei ${tempoEmHoras} hora.`)*/
+
+
+/**
+ * gora:
+
+Desafio 7 — 16:15 → 17:30
+
+Você entrou:
+
+16:15
+
+Saiu:
+
+17:30
+
+Pergunta:
+
+Quanto tempo você ficou no estacionamento?
+
+O resultado deve ser: agora:
+
+Desafio 7 — 16:15 → 17:30
+
+Você entrou:
+
+16:15
+
+Saiu:
+
+17:30
+
+Pergunta:
+
+Quanto tempo você ficou no estacionamento?
+
+O resultado deve ser:gora:
+
+Desafio 7 — 16:15 → 17:30
+
+Você entrou:
+
+16:15
+
+Saiu:
+
+17:30
+
+Pergunta:
+
+Quanto tempo você ficou no estacionamento?
+
+O resultado deve ser: 1 hora e 15 minutos
+ */
+
+const horaEntrada = 16
+const minutoEntrada = 15
+
+const horaSaida = 17
+const minutoSaida = 30
+
+const horarioEntradaEmminutos = (horaEntrada * 60) + minutoEntrada
+const horarioSaidaEmMinutos = (horaSaida * 60) + minutoSaida
+
+const comparaMinutos = horarioSaidaEmMinutos - horarioEntradaEmminutos
+
+// QUANTAS HORAS COMPLETAS CABEM EM 75MIN ?
+const qtdHoras = Math.floor(comparaMinutos / 60)
+
+// QUANTOS MINUTOS SOBRAM?
+const qtdMinutos = comparaMinutos % 60
+
+console.log(`${qtdHoras} horas e ${qtdMinutos} minutos`)
