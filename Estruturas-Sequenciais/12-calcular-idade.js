a141dcbbb2e3ac34728f54f9cbfe5adb460533d4
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-08-26 11:42:45 -0300

    feat: alteração exercicio calcula idade

--- Estruturas-Sequenciais/12-calcular-idade.js
+++ Estruturas-Sequenciais/12-calcular-idade.js
@@ -1,11 +1,13 @@
 /**
  * receba um ano de nascimento e informe a idade 
  */
+const prompt = require('prompt-sync')()
 
+let anoAtual = Number(prompt('Digite ano atual: '))
+let anoNacimento = Number(prompt('Digite ano do seu nascimento: '))
 
-let anoNascimento = 1989
-let anoAtual = 2026
+let idade = anoAtual - anoNacimento
 
-console.log(`sua idade é = ${anoAtual - anoNascimento } anos`)
+console.log(`sua idade é = ${idade} anos`)
 
 
