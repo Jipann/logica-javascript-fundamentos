38a2ecd7c28a20a7e1a8c9da7445b088797a53d1
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-08-26 12:17:57 -0300

    feat: alteração exercicio conversor mooeda

--- Estruturas-Sequenciais/06-conversor-moeda.js
+++ Estruturas-Sequenciais/06-conversor-moeda.js
@@ -2,10 +2,11 @@
  * Declare um valor em Reais (BRL) e a cotação atual do Dólar (USD); 
  * calcule e exiba o valor convertido em Dólares.
  */
+const prompt = require('prompt-sync')()
 
-const valoremReais = 100
-const cotacaoDolar = 5.14
+let valorEmReais = Number(prompt('Informe o valor em reais:'))
+let valorEmDolares = Number(prompt('Informe a cotação do dolar:'))
 
-const cambio = valoremReais / cotacaoDolar
+let cambio = valorEmReais / valorEmDolares
 
-console.log(`o montante de R$ ${valoremReais} convertido para Dolar é ${cambio.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}`)
+console.log(`o montante de R$ ${valorEmReais} convertido para Dolar é ${cambio.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}`)
