d1b64d2f791c534d12b0217badc79798613b4fc3
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-08-28 14:50:18 -0300

    feat: Atualização do exercicio media simples

--- Estruturas-Sequenciais/02-media-simples.js
+++ Estruturas-Sequenciais/02-media-simples.js
@@ -1,10 +1,16 @@
-/*Receba três notas de um aluno, 
-Objetivo: calcular a média aritmética e exibir o valor formatado. */
+/**
+ * Peça três notas ao usuário.
 
-const nota1 = 8.5
-const nota2 = 7.0
-const nota3 = 9.0
+    Calcule a média.
+ */
 
-const CalculaMedia = (nota1 + nota2 + nota3) / 3
+const prompt = require('prompt-sync')()
+
+let nota1 = Number(prompt('Informe a primeira nota: '))
+let nota2 = Number(prompt('Informe a primeira nota 3 : '))
+let nota3 = Number(prompt('Informe a primeira nota 3 : '))
+
+
+let CalculaMedia = (nota1 + nota2 + nota3) / 3
 
 console.log(`A media final do aluno é = ${CalculaMedia.toFixed(2)}`)
