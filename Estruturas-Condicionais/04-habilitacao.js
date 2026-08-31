0b8669487ac0658e774daf86e853e7fb9772a2a4
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-08-31 15:57:04 -0300

    feat: atualização exercicio habilitação

--- Estruturas-Condicionais/04-habilitacao.js
+++ Estruturas-Condicionais/04-habilitacao.js
@@ -1,13 +1,24 @@
 const prompt = require('prompt-sync')()
 
-let anoNascimento = Number(prompt('Informe o ano do seu nascimento:'))
+console.log('------------------------------')
+console.log('DEPARTAMENTO DE TRANSITO')
+console.log('------------------------------')
+
 let anoAtual = Number(prompt('Informe o ano atual:'))
+let anoNascimento = Number(prompt('Informe o ano do seu nascimento:'))
 
 let idade = anoAtual - anoNascimento
 
 if (idade >= 18) {
-    console.log(' Apto para tirar a habilitação! ')
 
+    console.log('------ STATUS ------')
+    console.log(`${idade} Anos`)
+    console.log(' Apto para tirar a habilitação! ')
+    console.log('------------------------------')
 }else{
+    console.log('------ STATUS ------')
+    console.log(`${idade} Anos`)
     console.log('não está apto para fazer habilitação! ')
+    console.log('------------------------------')
+    
 }
