13cf61bfc541375faec4721e705f443d28386664
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-08-31 15:43:07 -0300

    feat: exercicio para tirar habilitação

---
+++ Estruturas-Condicionais/05-media-aluno.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')()
+
+let nota1 = Number(prompt('Informe a nota 1: '))
+let nota2 = Number(prompt('Informe a nota 2: '))
+let nota3 = Number(prompt('Informe a nota 3: '))
+
+let media = (nota1 + nota2 + nota3) / 3
+
+if (media < 5) {
+    console.log('Aluno reprovado! ')
+
+}else if(media < 7 ){
+    console.log('Aluno em recuperação!')
+
+}else{
+    console.log('Aluno Aprovado! ')
+}
