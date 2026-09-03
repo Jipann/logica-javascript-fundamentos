085beecf26218ab55f3813585a75d83ea717867d
Leonardo Fontoura <leonardozfontoura@gmail.com>
2026-09-02 21:24:07 -0300

    fix: ajusta exibicao do exercicio de habilitacao

--- Estruturas-Condicionais/04-habilitacao.js
+++ Estruturas-Condicionais/04-habilitacao.js
@@ -11,12 +11,12 @@ let idade = anoAtual - anoNascimento
 
 if (idade >= 18) {
 
-    console.log('------ STATUS ------')
+    console.log('------- STATUS -------')
     console.log(`${idade} Anos`)
     console.log(' Apto para tirar a habilitação! ')
     console.log('------------------------------')
 }else{
-    console.log('------ STATUS ------')
+    console.log('------- STATUS -------')
     console.log(`${idade} Anos`)
     console.log('não está apto para fazer habilitação! ')
     console.log('------------------------------')
